4b880205d34b34a42ae2157bdc7fbf3acbfa40c3
Abdullah Mohammed <67126885+abdullah0707@users.noreply.github.com>
2020-07-27 17:04:24 +0200

    Add files via upload

--- src/components/UsersForm.js
+++ src/components/UsersForm.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {Formik, Form, Field, ErrorMessage} from "formik";
 import * as Yup from "yup";
+import {Prompt} from "react-router-dom"; //Define properties on Tag
 
 export default function UsersForm(props){
+    // Function Error Message View of Inbut
     const schema = Yup.object().shape({
         name: Yup.string().required(),
-        email: Yup.string().email().required()
+        email: Yup.string().email().required(),
+        phone: Yup.number().required(),
+        username: Yup.string().required(),
+        website: Yup.string().required()
     });
 
+    // Form Edit Date
     return <Formik 
         enableReinitialize={true}
         initialValues={props.values}
@@ -15,6 +21,8 @@ export default function UsersForm(props){
         validationSchema={schema}
         render={props=>{
             return <Form>
+                <Prompt when={props.dirty} message={'sure?'} />
+
                 <label>Name : </label>
                 <Field name="name" />
                 <ErrorMessage name="name" /><br/>
@@ -23,9 +31,22 @@ export default function UsersForm(props){
                 <Field name="email" />
                 <ErrorMessage name="email" /><br/>
 
+                <label>Phone : </label>
+                <Field name="phone" />
+                <ErrorMessage name="phone" /><br/>
+
+                <label>Username : </label>
+                <Field name="username" />
+                <ErrorMessage name="username" /><br/>
+
+                <label>Website : </label>
+                <Field name="website" />
+                <ErrorMessage name="website" /><br/>
+
                 <button type="submit">Send</button>
             </Form>
         }}
     />
 
 }
+
--- src/components/ViewUser.js
+++ src/components/ViewUser.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import {withRouter} from "react-router-dom";
 
-export default function (props) {
+// Props of View Data
+const ViewUser = function (props) {
     return <div>
+            Page: {props.location.pathname}<br />
+            Id: {props.match.params.id}
+
             <div>
                 Name: {props.user.name}
-            </div>
+            </div><br />
             <div>
                 Email: {props.user.email}
+            </div><br />
+            <div>
+                phone: {props.user.phone}
+            </div><br />
+            <div>
+                username: {props.user.username}
+            </div><br />
+            <div>
+                website: {props.user.website}
             </div>
         </div>;
 }
+
+export default withRouter(ViewUser);
