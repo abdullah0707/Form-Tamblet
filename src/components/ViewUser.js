cda509c85e1117fd34df93d943dcd1a61cc0d556
Abdullah Mohammed <67126885+abdullah0707@users.noreply.github.com>
2020-07-27 17:07:10 +0200

    Add files via upload

--- src/components/ViewUser.js
+++ src/components/ViewUser.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import {withRouter} from "react-router-dom";
 
-// Props of View Data
-const ViewUser = function (props) {
+export default function (props) {
     return <div>
-            Page: {props.location.pathname}<br />
-            Id: {props.match.params.id}
-
             <div>
                 Name: {props.user.name}
-            </div><br />
+            </div>
             <div>
                 Email: {props.user.email}
-            </div><br />
-            <div>
-                phone: {props.user.phone}
-            </div><br />
-            <div>
-                username: {props.user.username}
-            </div><br />
-            <div>
-                website: {props.user.website}
             </div>
         </div>;
 }
-
-export default withRouter(ViewUser);
